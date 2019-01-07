43777732e92581b20896268082f0b69ff7c4592c
Trevor Stanley <38226475+T-Man-Stan@users.noreply.github.com>
2019-01-07 16:54:10 -0700

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -96,7 +96,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() public payable {
+    function() external payable {
         revert();
     }
 }
